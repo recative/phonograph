b54e6248e34b1a9d3f96038e8559fb9f029c3412
Ben Pig Chu <benpichu@gmail.com>
2022-09-01 14:47:44 +0800

    Fix build system

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,47 @@
-import typescript from 'rollup-plugin-typescript';
-import pkg from './package.json';
+import dts from "rollup-plugin-dts";
+import resolve from "@rollup/plugin-node-resolve";
+import sucrase from '@rollup/plugin-sucrase';
+import external from "rollup-plugin-peer-deps-external";
 
-export default {
-	input: 'src/index.ts',
-	output: [
-		{ file: pkg.module, format: 'es', sourcemap: true },
-		{ file: pkg.main, format: 'umd', name: 'Phonograph', sourcemap: true }
-	],
-	plugins: [
-		typescript({
-			typescript: require('typescript')
-		})
-	]
-};
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external({
+        includeDependencies: true
+      }),
+      resolve({
+        extensions: ['.js', '.ts']
+      }),
+      sucrase({
+        exclude: ['node_modules/**'],
+        transforms: ['typescript']
+      }),
+    ]
+  },
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.types,
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+    plugins: [dts()],
+  },
+];
